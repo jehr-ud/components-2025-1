48e5f913049197d7ebcda1bcb2499fe928b7185c
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2025-04-03 07:58:56 -0500

    Main elements of app

--- 2. hanged game/app/src/main/java/com/ud/hangedgame/MainActivity.kt
+++ 2. hanged game/app/src/main/java/com/ud/hangedgame/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.ud.hangedgame
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import com.ud.hangedgame.ui.theme.HangedGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             HangedGameTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Game(
+                        words = "________",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Game(words: String, modifier: Modifier = Modifier) {
+    var texto by remember { mutableStateOf("") }
+    val contexto = LocalContext.current
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Descripción de la imagen"
+    )
     Text(
-        text = "Hello $name!",
+        text = "$words!",
         modifier = modifier
     )
+    TextField(
+        value = texto,
+        onValueChange = { texto = it },
+        label = { Text("Give me a character") }
+    )
+    Button(onClick = {
+        Toast.makeText(contexto, "¡Botón pulsado!", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Haz clic aquí")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HangedGameTheme {
-        Greeting("Android")
+        Game("Android")
     }
 }
