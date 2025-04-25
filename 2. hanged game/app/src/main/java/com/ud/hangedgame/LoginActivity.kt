8b2f03008cda8ab17bd4d4e891e3e6bffc2eeb3b
Juan Moreno <juan.morenodx@gmail.com>
2025-04-25 07:34:08 -0500

    texField se agrego para correo y contraseña

--- 2. hanged game/app/src/main/java/com/ud/hangedgame/LoginActivity.kt
+++ 2. hanged game/app/src/main/java/com/ud/hangedgame/LoginActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ud.hangedgame.ui.theme.HangedGameTheme
 
 
@@ -25,13 +27,38 @@ class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent{
-            Column{
+            Column(modifier = Modifier
+                .padding(top = 40.dp)
+                .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text("Bienvenido",
+                    fontSize = 32.sp
+                    )
+
+                val correo = remember { mutableStateOf("") }
+                val password = remember { mutableStateOf("") }
+                TextField(
+                    value = correo.value,
+                    onValueChange = { correo.value = it },
+                    label = { Text("Correo") }
+                )
+
+                TextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    label = { Text("Contraseña") }
+                )
+
                 Button(onClick = {
                     startActivity(Intent(this@LoginActivity, LevelActivity::class.java))
                 }) {
                     Text("Login")
                 }
+
             }
         }
     }
